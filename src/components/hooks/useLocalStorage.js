403b35c9e36e8f2e715886d7a035423cf0c9e90c
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-10-30 15:59:56 -0400

    almost there, dark mode not working yet

--- src/components/hooks/useLocalStorage.js
+++ src/components/hooks/useLocalStorage.js
@@ -1,14 +1,14 @@
 import { useState } from 'react';
 
 export const useLocalStorage = (key, initialValue) => {
-    const [storedValue, setStoredValue] = uesState(() => {
+    const [storedValue, setStoredValue] = useState(() => {
         const item = window.localStorage.getItem(key);
         return item ? JSON.parse(item) : initialValue;
     });
-    return [storedValue];
-
+    
     const setValue = value => {
-        setStoredValue(value);
-        window.localStorage.setItem(key, JSON.stringify(value));
+            window.localStorage.setItem(key, JSON.stringify(value));
+            setStoredValue(value);
     }
+    return [storedValue,setValue];
 };
